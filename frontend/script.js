1175101cff8c4c0d738843d760b3c5ce12df606e
Van <97835586+VanCarlo95@users.noreply.github.com>
2025-02-17 22:49:18 +0800

    s

--- frontend/script.js
+++ frontend/script.js
@@ -129,7 +129,7 @@ function scrollToBottom() {
 
 function sendMessageToServer(message) {
   // const url = 'http://localhost:5005/chat';
-  const url = "http://msubot-latest-production.up.railway.app";
+  const url = "https://msubot-latest-production.up.railway.app";
   console.log("Sending message to server:", { message });
 
   fetch(url, {
